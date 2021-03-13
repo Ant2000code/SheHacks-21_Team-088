7eea09167454f8e99569839c9904a65b0e093404
Ant2000code <59964873+Ant2000code@users.noreply.github.com>
2021-03-13 16:14:40 +0530

    initial commit

---
+++ user/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path,include
+from . import views
+
+
+
+urlpatterns = [
+    path('',views.userpage,name="user"),
+    path('createprofile',views.createprofile,name="createprofile"),
+    path('apply',views.apply,name="apply"),
+    path('createEvent',views.createEvent,name="createEvent"),
+    path('myCreatedEvents',views.myCreatedEvents,name="myCreatedEvents"),
+    path('eventDetails/<int:id>',views.eventDetails,name="eventDetails"),
+    path('myeventDetailed/<int:id>',views.myeventDetailed,name="myeventDetailed"),
+    path('deleteEvent/<int:id>',views.deleteEvent,name="deleteEvent"),
+    path('viewEvents',views.viewEvents,name="viewEvents"),
+    path('appliedEvents',views.appliedEvents,name="appliedEvents"),
+    path('addMember/<int:id>',views.addMember,name="addMember"),
+    path('contact/', views.contact, name='contact'), 
+    path('userpage/',views.userpg,name="userpg"),
+]
